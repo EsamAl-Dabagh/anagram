79ca41bac5a0f556ac80157891e6875db6b18de0
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-12-06 14:31:23 +0000

    fix: delete .rstrip from parsing of wordlist txt file

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -2,7 +2,18 @@ class WordList
 
   attr_reader :source
 
-  def initialize(source = "../wordlist.txt")
+  def initialize(source = "../anagrams/wordlist.txt")
     @source = source
+  end
+
+  def generate 
+
+    anagrams = []
+
+    File.foreach(@source) do | word |
+      anagrams << word.chomp
+    end
+
+    anagrams
   end
 end
--- spec/word_list_spec.rb
+++ spec/word_list_spec.rb
@@ -1,9 +1,17 @@
 require_relative "../lib/word_list.rb"
 
 describe WordList do
-  let(:word_list) { described_class.new }
+  let(:word_list_default) { described_class.new }
+  let(:word_list) {described_class.new("../anagrams/spec/support/test-wordlist.txt")}
 
   it "initializes with a default path to wordlist file" do
-    expect(word_list.source).to eq("../wordlist.txt")
+    expect(word_list_default.source).to eq("../anagrams/wordlist.txt")
   end
+
+  describe "#generate" do
+    it "returns an array of words" do
+      test_list = ["Americano", "Americans", "Americas", "Americus"]
+      expect(word_list.generate).to eq(test_list)
+    end
+  end 
 end
